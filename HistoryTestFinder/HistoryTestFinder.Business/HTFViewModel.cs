4dc9a5b722b659f0756663e4b13f48ce7ba1ddfc
Tomer Gal <gt250115@ncr.com>
2022-10-03 15:55:07 +0300

    Added Command to Submit button

--- HistoryTestFinder/HistoryTestFinder.Business/HTFViewModel.cs
+++ HistoryTestFinder/HistoryTestFinder.Business/HTFViewModel.cs
@@ -1,6 +1,43 @@
-﻿namespace HistoryTestFinder.Business
+﻿using System;
+using System.Windows.Input;
+
+namespace HistoryTestFinder.Business
 {
 	public class HTFViewModel
 	{
+		private ICommand _submitCommand;
+
+		public ICommand SubmitCommand
+		{
+			get
+			{
+				if (_submitCommand == null)
+					_submitCommand = new Updater();
+				return _submitCommand;
+			}
+			set
+			{
+				_submitCommand = value;
+			}
+		}
+	}
+
+	public class Updater : ICommand
+	{
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public void Execute(object parameter)
+		{
+
+		}
 	}
 }
